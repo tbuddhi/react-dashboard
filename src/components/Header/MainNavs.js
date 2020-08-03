600c8ecaa2c1935b21d3d0d7d7f03edfe7936481
Tharaka Buddhi <tharakabuddhi@gmail.com>
2020-08-04 02:12:48 +0530

    delete un-used code

--- src/components/Header/MainNavs.js
+++ src/components/Header/MainNavs.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom'
 
 const NavWrapper = styled.div`
     display: block;
